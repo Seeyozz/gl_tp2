00b0705dda04cabe8a0623dee09afbf3a28a817a
Benjamin <benjamin.kalfa@efrei.net>
2020-10-23 15:12:34 +0200

    Revert "add #define for texts"
    This reverts commit ff2b2d3be8c151303deb4a75a8df6e8c358bbf8f.

--- FizzBuzz.c
+++ FizzBuzz.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define FIZZ "Fizz"
-#define BUZZ "Buzz"
-
 int main(void){
     for (int i=1; i<=100; i++){
         if(i%15==0){
-            printf("%s%s\n", FIZZ, BUZZ);
+            printf("FizzBuzz\n");
         } else if (i%3==0){
-            printf("%s\n", FIZZ);
+            printf("Fizz\n");
         } else if (i%5 == 0){
-            printf("%s\n", BUZZ);
+            printf("Buzz\n");
         }else{
             printf("%d\n", i);
         }
